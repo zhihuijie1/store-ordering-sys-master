54bc732072cd74e19804a7a57db41fda2d873942
陈广杰 <92995461+zhihuijie1@users.noreply.github.com>
2024-07-20 20:31:42 +0800

    sky-common

---
+++ sky-common/src/main/java/com/sky/exception/PasswordErrorException.java
@@ -0,0 +1,14 @@
+package com.sky.exception;
+
+/**
+ * 密码错误异常
+ */
+public class PasswordErrorException extends BaseException {
+
+    public PasswordErrorException() {
+    }
+
+    public PasswordErrorException(String msg) {
+        super(msg);
+    }
+}
